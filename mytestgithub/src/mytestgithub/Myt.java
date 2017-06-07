5ed098fd6d6601a5dc82dd7b5c3befcfc46a7e6f
yujianghai <yujiangh@hqauto.com.cn>
2017-06-07 16:57:33 +0800

    2017

---
+++ mytestgithub/src/mytestgithub/Myt.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) wpjava@163.com 2014 All Rights Reserved.
+ */
+package mytestgithub;
+
+/**
+ *                       
+ * @Filename: Myt.java
+ * @Version: 1.0
+ * @Author: Õı≈Û
+ * @Email: wangpeng@ejavashop.com
+ *
+ */
+public class Myt {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
